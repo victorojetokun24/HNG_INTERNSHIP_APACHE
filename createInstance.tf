6917794be54123a374e3cc7a9b5b136103a12267
Victor Ojetokun <victorojetokun24@gmail.com>
2024-06-29 22:22:03 +0100

    modify

---
+++ createInstance.tf
@@ -0,0 +1,60 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = file("~/path/to/your/id_rsa.pub")
+}
+
+resource "aws_security_group" "web_sg" {
+  name_prefix = "web-sg-"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0c819f65440d5f1d1"
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.deployer.key_name
+  security_groups = [aws_security_group.web_sg.name]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt-get update -y
+              sudo apt-get install -y apache2 git
+              sudo systemctl start apache2
+              sudo systemctl enable apache2
+              cd /var/www/html
+              sudo rm -rf *
+              sudo git clone 
+              sudo systemctl restart apache2
+              EOF
+
+  tags = {
+    Name = "WebServer"
+  }
+}
+
+output "instance_ip" {
+  value = aws_instance.web.public_ip
+}
